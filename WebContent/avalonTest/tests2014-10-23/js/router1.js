83a09464806d273fd9d3de7b75486ad16765c6e9
Administrator <Administrator@PC-201407231815>
2014-10-23 15:33:17 +0800

    路由测试

---
+++ WebContent/avalonTest/tests2014-10-23/js/router1.js
@@ -0,0 +1,4 @@
+avalon.define( "router1Controller", router1Controller );
+function router1Controller( vm ) {
+	vm.values = ["我是路由1.1号","我是路由1.2号","我是路由1.3号"];
+}
---
+++ WebContent/avalonTest/tests2014-10-23/js/router2.js
@@ -0,0 +1,4 @@
+avalon.define( "router2Controller", router2Controller );
+function router2Controller( vm ) {
+	vm.values = ["我是路由2.1号","我是路由2.2号","我是路由2.3号"];
+}
